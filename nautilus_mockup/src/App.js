f88c8dc15697b4f7171e4b2e848312eebe11c244
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-26 12:36:59 -0400

    Got matches pic and title.

--- nautilus_mockup/src/App.js
+++ nautilus_mockup/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./App.css";
 import matches from "./assets/matches.png";
+// Bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>1 of 1</Col>
+    <Container fluid>
+      <Row className="title">
+        <Col>
+          <div className="title-text">
+            <Carousel.Caption>
+              <h3>ISSUE 79</h3>
+              <h1>Catalyst</h1>
+              <h4>Agents of change</h4>
+            </Carousel.Caption>
+          </div>
+          <Image className="matches" src={require("./assets/matches.png")} fluid />
+        </Col>
       </Row>
     </Container>
   );
